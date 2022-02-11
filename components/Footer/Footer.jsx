acb86de4a54d3e5e00ff2d6b04250131fb4b396e
manish <manishvyas393@gmail.com>
2022-02-11 17:38:34 +0530

    footer responsiveness fixed

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 import NavLink from "../NavLink/NavLink"
-import { Flex, Box, Heading,Text} from "@chakra-ui/react"
+import { Flex, Box, Heading, Text } from "@chakra-ui/react"
 const Footer = () => {
       return (
-            <Flex display="flex" flexDirection={{lg:"row",sm:"column"}} w="100%" backgroundColor={["gray.100"]} height={350} p={{lg:"50px",sm:"5px"}}  mt="5" justifyContent="center" alignItems="center" shadow="inner">
-                  <Box display="flex" w={{lg:"50%",sm:"100%"}} px={{lg:5,sm:1}} justifyContent="space-around" mb={5} mt={10}>
-                        <Box display="flex" flexDirection="column" w={{sm:"50%"}}>
+            <Box display="flex" flexDirection={{ lg: "row", sm: "column" ,md:"row",xs:"column"}} w={{lg:"100%",sm:"124%",md:"100%",xs:"205%"}} backgroundColor={["gray.100"]} height={350} p={{ lg: "50px", sm: "10px" }} mt="5" justifyContent="center" alignItems="center" shadow="inner">
+                  <Box display="flex" w={{ lg: "50%", sm: "100%", xs: "100%" }} px={{ lg: 54, sm: 0 }} justifyContent="space-around" mt={6}>
+                        <Box display="flex" flexDirection="column">
                               <Text>logo</Text>
                               <Text>© 2020.All rights reserved</Text>
                         </Box>
                         <Box display="flex" flexDirection="column">
                               <Heading as="h5" fontSize={{ sm: 28, lg: 35, md: 25 }} fontWeight={{ sm: 300 }} mb={4}>Products</Heading>
-                              <NavLink href="/" name='Overview' fontSize={{ sm: "16px", lg: "17px", md: "18px" }}/>
+                              <NavLink href="/" name='Overview' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
                               <NavLink href="/" name='Features' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
-                              <NavLink href="/" name='pricing' fontSize={{ sm: "16px", lg: "17px", md: "18px" }}/>
-                            
+                              <NavLink href="/" name='pricing' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
+
                         </Box>
                   </Box>
-                  <Box display="flex" w={{ lg: "50%", sm: "120%" }} px={{ lg: 5, sm: 1 }} justifyContent="space-around" mt={6}>
+                  <Box display="flex" w={{ lg: "50%", sm: "100%",xs:"100%" }} px={{ lg: 54, sm:0}} justifyContent="space-around" mt={6}>
                         <Box display="flex" flexDirection="column">
-                              <Heading as="h5" fontSize={{ sm: 28, lg: 35, md: 25 }} fontWeight={{sm:300}} mb={4}>Support</Heading>
-                              <NavLink href="/" name='Help Centre' fontSize={{ sm: "16px", lg: "17px", md: "18px" }}/>
+                              <Heading as="h5" fontSize={{ sm: 28, lg: 35, md: 25 }} fontWeight={{ sm: 300 }} mb={4}>Support</Heading>
+                              <NavLink href="/" name='Help Centre' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
                               <NavLink href="/" name='Terms Of Service' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
                               <NavLink href="/" name='Privacy Policy' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
                         </Box>
                         <Box display="flex" flexDirection="column">
                               <Heading as="h5" fontSize={{ sm: 28, lg: 35, md: 25 }} fontWeight={{ sm: 300 }} mb={4}>Follow Us</Heading>
-                              <NavLink href="/" name='Instagram' fontSize={{ sm: "16px", lg: "17px", md: "18px" }}/>
-                              <NavLink href="/" name='LinkedIn' fontSize={{ sm: "16px", lg: "17px", md: "18px" }}/>
-                              <NavLink href="/" name='Facebook' fontSize={{ sm: "16px", lg: "17px", md: "18px" }}/>
+                              <NavLink href="/" name='Instagram' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
+                              <NavLink href="/" name='LinkedIn' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
+                              <NavLink href="/" name='Facebook' fontSize={{ sm: "16px", lg: "17px", md: "18px" }} />
                         </Box>
 
                   </Box>
-            </Flex>
+            </Box>
       )
 }
 
